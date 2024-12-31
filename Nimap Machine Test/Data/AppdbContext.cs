3cc641e7c67ddae30c90cfc38aa1c752f2b65314
Monali305 <108350054+Monali305@users.noreply.github.com>
2024-12-31 17:31:14 +0530

    Update AppdbContext.cs

--- Nimap Machine Test/Data/AppdbContext.cs
+++ Nimap Machine Test/Data/AppdbContext.cs
@@ -1,9 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Nimap_Machine_Test.Models;
 
 namespace Nimap_Machine_Test.Data
 {
-    public class AppdbContext:DbContext
+    public class AppdbContext : DbContext
     {
         //Constructor
         public AppdbContext(DbContextOptions<AppdbContext> options) : base(options)
@@ -13,5 +13,16 @@ namespace Nimap_Machine_Test.Data
         //DbSet properties
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>()
+                .HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId)
+                .OnDelete(DeleteBehavior.Cascade); // Ensure Cascade Delete
+        }
     }
 }
